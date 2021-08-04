07708df7d38149c7012b37ba498824f4e8fe916a
Indrajeet <44095180+Indra2311@users.noreply.github.com>
2021-08-04 23:46:14 +0530

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,6 @@
+  
+source /home/ubuntu/chat-env/bin/activate
+cd /home/ubuntu/new_chatapp
+pip3 install django bcrypt django-extensions
+pip3 install -r /home/ubuntu/new_chatapp/requirements.txt
+pip3 install gunicorn
